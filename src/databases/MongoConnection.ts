7ed03571fd3f00bb0deb174ef420b5ec9174a33e
DaYuan <sduum82@gmail.com>
2021-04-04 06:01:46 +0800

    fix : 🕹add more connection

--- src/databases/MongoConnection.ts
+++ src/databases/MongoConnection.ts
@@ -4,7 +4,7 @@ import config from '../config';
 
 // const uri = 'mongodb://mongo:27017/test-Dcard-01';
 // const uri = `mongodb://127.0.0.1:27017/test-Dcard-01`;
-const uri = `mongodb://${config.mongodb}:27017/test-Dcard-01`;
+const uri = `mongodb://${config.mongodb}:27017/test-Dcard`;
 
 const connectOptions = {
   useNewUrlParser: true,
@@ -16,4 +16,32 @@ export const connectToMongo = () => {
   mongoose.connect(uri, connectOptions, () => {
     logger.info('connected to database');
   });
+  mongoose.Promise = Promise; // yea... this is kinda odd 
+
+  mongoose.connection.on("connected", () => {
+      console.log("Connection Established");
+  });
+
+  mongoose.connection.on("reconnected", () => {
+      console.log("Connection Reestablished");
+  });
+
+  mongoose.connection.on("disconnected", () => {
+      console.log("Connection Disconnected");
+  });
+
+  mongoose.connection.on("close", () => {
+      console.log("Connection Closed");
+  });
+
+  mongoose.connection.on("error", (error) => {
+      console.log("ERROR: " + error);
+  });
+  return mongoose;
 };
+export const connectedMongo = () => {
+  mongoose.disconnect(function(){
+    mongoose.connection.close();
+  });
+};
+
